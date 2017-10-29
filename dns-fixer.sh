0bb4357742b412e68f7fe3354205fecdc43e7535
Pouya Abbasian <matprogramm3r@gmail.com>
2017-10-30 00:42:39 +0330

    Fixed Script

--- dns-fixer.sh
+++ dns-fixer.sh
@@ -3,31 +3,31 @@
 var1=`ip addr | grep brd | wc -l`
 var2=`ip addr | awk {'print $1'} | grep : | wc -l`
 if [ "$var1" == "$var2" ];then
-    echo -e "\033[91m[-]Your Are Offline!!!"
-    exit 1
+	echo -e "\033[91m[-]Your Are Offline!!!"
+	exit 1
 else
-    echo -e "\033[92m[+]Your Local Netowkr Is Ok!."
+	echo -e "\033[92m[+]Your Local Netowkr Is Ok!."
 ##start internet Checking
 	ping -c2 -w2 8.8.8.8 1>/dev/null 2>/dev/null
 	if [ "$?" != "0" ];then
-	    echo -e "\033[91m[-]You Have Internet ERROR! CHECK THAT."
+		echo -e "\033[91m[-]You Have Internet ERROR! CHECK THAT."
 		exit 1
 	else
-	    echo -e "\033[92m[+]Your Internet Is OK!"
+		echo -e "\033[92m[+]Your Internet Is OK!"
 		##Start DNS Checking
 		nslookup gnu.org -timeout=1 1>/dev/null 2>/dev/null 1>/dev/null>null 2>/dev/null 
 		if [ "$?" != "0" ];then
-		    echo -e "\033[91m[-]You Have Dns ERROR! "
-		    read -p "I cat To Ok That, Do You Want To Contniue?[Y/n] " yesorno
-		    if [ "$yesorno" == "Y" ] || [ "$yesorno" == "y" ] || [ "$yesorno" == "" ];then
-		    	echo "nameserver 208.67.222.222\nnameserver 8.8.8.8" | sudo tee /etc/resolvconf/resolv.conf.d/tail 1>/dev/null
+			echo -e "\033[91m[-]You Have Dns ERROR! "
+			read -p "I cat To Ok That, Do You Want To Contniue?[Y/n] " yesorno
+			if [ "$yesorno" == "Y" ] || [ "$yesorno" == "y" ] || [ "$yesorno" == "" ];then
+				echo "nameserver 208.67.222.222\nnameserver 8.8.8.8" | sudo tee /etc/resolvconf/resolv.conf.d/tail 1>/dev/null
 				sudo systemctl restart NetworkManager
 			else
 				echo "Ok, You Need To Fix it." 
 				exit 1
-		    fi
+			fi
 		else
-    		echo -e "\033[92m[+]Your DNS Is OK!"
+			echo -e "\033[92m[+]Your DNS Is OK!"
 			exit 0
 		fi
 ##End Of DNS Checking.
